f51e9970056a060972c919f9bbda26886e80478e
rodrigocdf <rodrigo.fernandes@cdf.net>
2022-03-04 20:50:21 -0300

    Incluido nova rota para calculo da media por car_make

--- src/infra/http/route/routes.py
+++ src/infra/http/route/routes.py
@@ -3,6 +3,7 @@ from src.infra.utility.http import Http
 import src.infra.http.auth as auth
 import src.controller.arquivoController as arquivoController
 import src.controller.avg_car_make as avg_car_make
+import src.controller.avg_single_car_make as avg_single_car_make
 import flask
 
 
@@ -44,6 +45,18 @@ def route(app: flask.app.Flask):
 
             return response, 200
 
+        except Exception as err:
+            response = Http.handle_generic_http_error(err)
+
+            return jsonify(response), 500
+
+    @app.route('/avgsiglecarmake/<id_file>/carmake/<car_make>', methods=['GET'])
+    @auth.requires_auth
+    def request_get_avg_single_car_make(id_file,car_make):
+        try:
+            response = avg_single_car_make.avg_single_car_make(id_file,car_make)
+
+            return response, 200
         except Exception as err:
             response = Http.handle_generic_http_error(err)
 
