74fdf970f0dc10ec6e739c9c198c70b0c4b8a2de
Alan Oliveira <alan_oliveira14@hotmail.com>
2022-03-03 22:43:53 -0300

    incrementando salvamento do arquivo e gerando json de historico

--- src/controller/arquivoController.py
+++ src/controller/arquivoController.py
@@ -1,5 +1,8 @@
 import os
 import uuid
+import json
+from builtins import print
+
 
 def receiveFile(arquivo):
 
@@ -9,11 +12,30 @@ def receiveFile(arquivo):
 
     arquivo.save(os.path.join("uploadedFiles/", nomeUnicoArquivo))
 
-    print('gravação finalizada')
-
     response = {
         "idArquivo": nomeUnicoArquivo,
         "arquivoEnviado":arquivo.filename
     }
 
+    if os.path.isfile('auxiliar/arquivosEnviados.json'):
+        try:
+            with open('auxiliar/arquivosEnviados.json') as json_file:
+                data = json.load(json_file)
+                data["arquivos"].append(response)
+                print("incrementando json de registro")
+                gravaArquivo(data)
+        except Exception as ex:
+            print(ex)
+    else:
+        print("criando json de registro")
+        data = {'arquivos': [response]}
+        gravaArquivo(data)
+
+    print('gravação finalizada')
+
+
     return response
+
+def gravaArquivo(data):
+    with open('auxiliar/arquivosEnviados.json', 'w') as outfile:
+        json.dump(data, outfile, indent=2)
